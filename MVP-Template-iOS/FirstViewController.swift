5ba7556ac0a42fab4d46609992f470d39ae9496c
locallaghan <locallaghan@inhancetechnology.com>
2017-05-06 17:08:02 +0100

    Initial Commit

---
+++ MVP-Template-iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MVP-Template-iOS
+//
+//  Created by Liam O'Callaghan on 06/05/2017.
+//  Copyright © 2017 Liam O'Callaghan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
